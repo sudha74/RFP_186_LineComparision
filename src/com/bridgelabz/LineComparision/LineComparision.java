5815056fea6b2ddcaaf01a0cf16b52c278ae3d45
sudha123 <sudhanaukala1997@gmail.com>
2022-08-25 07:55:18 +0530

    checkequality

--- src/com/bridgelabz/LineComparision/LineComparision.java
+++ src/com/bridgelabz/LineComparision/LineComparision.java
@@ -14,7 +14,30 @@ public class LineComparision {
         int x2 = scanner.nextInt();
         System.out.println("Enter poits of y2");
         int y2 = scanner.nextInt();
-        double length = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
-        System.out.println("Length of line between two points: " + length);
+        Double length1 = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+        System.out.println("Length of first line: " + length1);
+
+        System.out.println("Enter points of x3");
+        int x3 = scanner.nextInt();
+        System.out.println("Enter points of y3");
+        int y3 = scanner.nextInt();
+        System.out.println("Enter points of x4");
+        int x4 = scanner.nextInt();
+        System.out.println("Enter points of y4");
+        int y4 = scanner.nextInt();
+        Double length2 = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+        System.out.println("Length of second line: " + length2);
+//        if (length1==length2) {
+//            System.out.println("Lines are equal");
+//        }else {
+//            System.out.println("Lines are not equal");
+
+        boolean result = length1.equals(length2);
+        if (result == true) {
+            System.out.println("Lines are equal");
+        } else {
+            System.out.println("Lines are not rqual");
+        }
+
     }
 }
